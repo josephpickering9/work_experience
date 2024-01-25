7340618d6981e6ff1ff78d5c4f9f8fac0375a25b
Joseph Pickering <josephpickering9@gmail.com>
2024-01-25 15:47:04 +0000

    ecosystem updates

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'WorkExperienceWeb',
-      port: '3000',
+      port: '3010',
       exec_mode: 'cluster',
       instances: 'max',
       script: './.output/server/index.mjs',
