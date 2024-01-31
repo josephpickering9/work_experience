7e5055e362627e0e2d71b10760a3e968b86cb9b4
Joseph Pickering <josephpickering9@gmail.com>
2024-01-31 12:53:33 +0000

    final

--- plugins/axios-config.ts
+++ plugins/axios-config.ts
@@ -2,8 +2,5 @@ import { OpenAPI } from '../api'
 import { defineNuxtPlugin, useRuntimeConfig } from '#app'
 
 export default defineNuxtPlugin(() => {
-  const config = useRuntimeConfig()
-  // eslint-disable-next-line no-console
-  console.log('config.public', config.public, process.env)
-  OpenAPI.BASE = process.env.NUXT_PUBLIC_API_BASE ?? ''
+  OpenAPI.BASE = useRuntimeConfig().public.apiBase
 })
