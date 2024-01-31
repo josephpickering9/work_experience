38f728168ccdce008f9a4c03476e4a885263c388
Joseph Pickering <josephpickering9@gmail.com>
2024-01-31 12:05:46 +0000

    console

--- plugins/axios-config.ts
+++ plugins/axios-config.ts
@@ -1,7 +1,9 @@
 import { OpenAPI } from '../api'
 import { defineNuxtPlugin, useRuntimeConfig } from '#app'
 
-export default defineNuxtPlugin((nuxtApp) => {
+export default defineNuxtPlugin(() => {
   const config = useRuntimeConfig()
+  // eslint-disable-next-line no-console
+  console.log('config.public.apiBase', config.public.apiBase)
   OpenAPI.BASE = config.public.apiBase
 })
