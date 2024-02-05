9e0b715045e2caf7d8506a5c9fb1b65eeb0b2c74
josephpickering9 <128248699+josephpickering9@users.noreply.github.com>
2024-02-05 21:43:24 +0000

    Deploy Speed (#19)

--- server/api/sitemap.ts
+++ server/api/sitemap.ts
@@ -2,7 +2,7 @@ import { OpenAPI, ProjectService } from '../../api'
 
 export default defineSitemapEventHandler(async (e) => {
   try {
-    OpenAPI.BASE = process.env.NUXT_PUBLIC_API_BASE ?? 'http://localhost:5105'
+    OpenAPI.BASE = process.env.NUXT_PUBLIC_API_BASE ?? useRuntimeConfig().public.apiBase ?? ''
     const projects = await ProjectService.getProject()
 
     return projects.map((p) => ({
