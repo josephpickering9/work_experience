800d79007566616ff97e7570d92ea2a88241e557
josephpickering9 <128248699+josephpickering9@users.noreply.github.com>
2024-02-10 13:55:53 +0000

    PWA (#28)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,6 +7,12 @@ export default defineNuxtConfig({
     head: {
       charset: 'utf-8',
       viewport: 'width=device-width, initial-scale=1',
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+        { rel: 'apple-touch-icon', href: '/apple-touch-icon.png' },
+        { rel: 'mask-icon', href: '/mask-icon.svg', color: '#1D232A' },
+      ],
+      meta: [{ name: 'theme-color', content: '#1D232A' }],
     },
     pageTransition: { name: 'page', mode: 'out-in' },
   },
@@ -22,7 +28,14 @@ export default defineNuxtConfig({
     },
   },
 
-  modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt', 'nuxt-icon', '@pinia-plugin-persistedstate/nuxt', 'nuxt-simple-sitemap'],
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@pinia/nuxt',
+    'nuxt-icon',
+    '@pinia-plugin-persistedstate/nuxt',
+    'nuxt-simple-sitemap',
+    '@vite-pwa/nuxt',
+  ],
 
   alias: {
     cookie: 'cookie',
@@ -31,5 +44,33 @@ export default defineNuxtConfig({
   sitemap: {
     sources: ['/api/sitemap'],
     exclude: ['/auth/*', '*/new'],
+  },
+
+  pwa: {
+    includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+    manifest: {
+      name: 'Work Experience',
+      short_name: 'Work Experience',
+      description: "Joseph Pickering | All of the projects that I've been involved in",
+      theme_color: '#1D232A',
+      icons: [
+        {
+          src: 'android-chrome-192x192.png',
+          sizes: '192x192',
+          type: 'image/png',
+        },
+        {
+          src: 'android-chrome-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+        },
+        {
+          src: 'android-chrome-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'any maskable',
+        },
+      ],
+    },
   },
 })
